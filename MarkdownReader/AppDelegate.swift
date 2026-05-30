78cc06606cf039cf0e2d680b4bb0f37e80b3da35
Manuel Olmos Gil <manuolmos88@gmail.com>
2026-05-30 19:17:03 -0300

    Initial Commit

---
+++ MarkdownReader/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MarkdownReader
+//
+//  Created by Manuel Olmos Gil on 30/05/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
