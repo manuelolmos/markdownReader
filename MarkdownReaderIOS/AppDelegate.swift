8c9285fc96fd6deb130bb1a8f084b8a06925e7ec
Manuel Olmos Gil <manuolmos88@gmail.com>
2026-06-22 11:38:26 -0300

    fix: adopt UIScene lifecycle to silence deprecation warning

--- MarkdownReaderIOS/AppDelegate.swift
+++ MarkdownReaderIOS/AppDelegate.swift
@@ -3,13 +3,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        window?.makeKeyAndVisible()
-        return true
+        true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
